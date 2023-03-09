77ee210aaacd7b98d61cf501dc74b3cb23bc9e18
Ananya <99496164+AnanyaM007@users.noreply.github.com>
2023-03-09 19:53:48 +0530

    changes

---
+++ src/pages/home.jsx
@@ -0,0 +1,175 @@
+import React from "react";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Typography,
+  Button,
+  IconButton,
+  Input,
+  Textarea,
+} from "@material-tailwind/react";
+import { UsersIcon } from "@heroicons/react/24/solid";
+import { PageTitle, Footer } from "@/widgets/layout";
+import { FeatureCard, TeamCard } from "@/widgets/cards";
+import { featuresData, teamData, contactData } from "@/data";
+
+export function Home() {
+  return (
+    <>
+      <div className="relative flex h-screen content-center items-center justify-center pt-16 pb-32">
+        <div className="absolute top-0 h-full w-full bg-[url('../../img/hero.jpg')] bg-cover bg-center" />
+        <div className="absolute top-0 h-full w-full bg-black/75 bg-cover bg-center" />
+        <div className="max-w-8xl container relative mx-auto">
+          <div className="flex flex-wrap items-center">
+            <div className="ml-auto mr-auto w-full px-4 text-center lg:w-8/12">
+              <Typography
+                variant="h1"
+                color="white"
+                className="mb-6 font-black"
+              >
+                Feel better about finding healthcare
+              </Typography>
+              <Typography variant="lead" color="white" className="opacity-80">
+               At HeartBeats, we take the responsibility of finding the best hospitals, healthcare facilities and medication that are suitable for you and your loved ones.
+              </Typography>
+            </div>
+          </div>
+        </div>
+      </div>
+      <section className="-mt-32 bg-gray-50 px-4 pb-20 pt-4">
+        <div className="container mx-auto">
+          <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+            {featuresData.map(({ color, title, icon, description }) => (
+              <FeatureCard
+                key={title}
+                color={color}
+                title={title}
+                icon={React.createElement(icon, {
+                  className: "w-5 h-5 text-white",
+                })}
+                description={description}
+              />
+            ))}
+          </div>
+          <div className="mt-32 flex flex-wrap items-center">
+            <div className="mx-auto -mt-8 w-full px-4 md:w-5/12">
+              <div className="mb-6 inline-flex h-16 w-16 items-center justify-center rounded-full bg-white p-3 text-center shadow-lg">
+                <UsersIcon className="h-6 w-6 text-blue-gray-900" />
+              </div>
+              <Typography
+                variant="h3"
+                className="mb-3 font-bold"
+                color="blue-gray"
+              >
+                Helping thousands find a health expert
+              </Typography>
+              <Typography className="mb-8 font-normal text-blue-gray-500">
+               In this era, with thousands of hospitals, medications and doctors, people often find it quite difficult to find a facility perfect for themselves. 
+                <br />
+                <br />
+                HeartBeats brings you the platform where you find a one-click solution to your issues by shortlisting the best facilities that match your criteria of quality treatment and consultation according to your financial conditions. We also provide methods to let people access their desired healthcare.
+              </Typography>
+              <Button variant="outlined">read more</Button>
+            </div>
+            <div className="mx-auto mt-24 flex w-full justify-center px-4 md:w-4/12 lg:mt-0">
+              <Card className="shadow-lg shadow-gray-500/10">
+                <CardHeader className="relative h-56">
+                  <img
+                    alt="Card Image"
+                    src="/img/teamwork.jpeg"
+                    className="h-full w-full"
+                  />
+                </CardHeader>
+                <CardBody>
+                  <Typography
+                    variant="h5"
+                    color="blue-gray"
+                    className="mb-3 font-bold"
+                  >
+                    Top Healthcare Specialists
+                  </Typography>
+                  <Typography className="font-normal text-blue-gray-500">
+                    We bring you the best care from the best hands of the world. Specialists in every field bring you the best care and consultations whenever you need.
+                  </Typography>
+                </CardBody>
+              </Card>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="px-4 pt-20 pb-48">
+        <div className="container mx-auto">
+          <PageTitle heading="Profound Specialists in every Field">
+            Starting from neurosurgery to orthopedics, our experts have been a constant support for the people in need providing them with best treatment.          </PageTitle>
+          <div className="mt-24 grid grid-cols-1 gap-12 gap-x-24 md:grid-cols-2 xl:grid-cols-4">
+            {teamData.map(({ img, name, position, socials }) => (
+              <TeamCard
+                key={name}
+                img={img}
+                name={name}
+                position={position}
+                socials={
+                  <div className="flex items-center gap-2">
+                    {socials.map(({ color, name }) => (
+                      <IconButton key={name} color={color} variant="text">
+                        <i className={`fa-brands text-lg fa-${name}`} />
+                      </IconButton>
+                    ))}
+                  </div>
+                }
+              />
+            ))}
+          </div>
+        </div>
+      </section>
+      <section className="relative bg-blue-gray-50/50 py-24 px-4">
+        <div className="container mx-auto">
+          <PageTitle heading="Quality Medication">
+           We provide details of medication recommended by our experts within the reach  of the patients for their convenience.
+          </PageTitle>
+          <div className="mx-auto mt-20 mb-48 grid max-w-5xl grid-cols-1 gap-16 md:grid-cols-2 lg:grid-cols-3">
+            {contactData.map(({ title, icon, description }) => (
+              <Card
+                key={title}
+                color="transparent"
+                shadow={false}
+                className="text-center text-blue-gray-900"
+              >
+                <div className="mx-auto mb-6 grid h-14 w-14 place-items-center rounded-full bg-white shadow-lg shadow-gray-500/20">
+                  {React.createElement(icon, {
+                    className: "w-5 h-5",
+                  })}
+                </div>
+                <Typography variant="h5" color="blue-gray" className="mb-2">
+                  {title}
+                </Typography>
+                <Typography className="font-normal text-blue-gray-500">
+                  {description}
+                </Typography>
+              </Card>
+            ))}
+          </div>
+          <PageTitle heading="Want to sponsor us?">
+           To help us make the best of our services
+          </PageTitle>
+          <form className="mx-auto mt-12 max-w-3xl text-center">
+            <div className="mb-8 flex gap-8">
+              <Input variant="standard" size="lg" label="Full Name" />
+              <Input variant="standard" size="lg" label="Email Address" />
+            </div>
+            <Textarea variant="standard" size="lg" label="Message" rows={8} />
+            <Button variant="gradient" size="lg" className="mt-8">
+              Send Message
+            </Button>
+          </form>
+        </div>
+      </section>
+      <div className="bg-blue-gray-50/50">
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default Home;
